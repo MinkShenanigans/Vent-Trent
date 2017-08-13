f4126eef68325c74931532f51e0ae296863e564d
MinkShenanigans <con_structor@yahoo.co.uk>
2017-08-13 05:50:15 +0100

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -26,22 +26,28 @@ namespace VentTrent
 
             string token = "TOKEN"; // Remember to keep this private!
             await client.LoginAsync(TokenType.Bot, token);
-            for (int i = 0; i < loadFile().Length; i++)
+            for (int i = 0; i < loadSlurList().Length; i++)
             {
-                Console.WriteLine(loadFile()[i] + "s");
-                Console.WriteLine(loadFile()[i]);
+                Console.WriteLine(loadSlurList()[i] + "s");
+                Console.WriteLine(loadSlurList()[i]);
             }
-            Console.WriteLine(loadFile());
+            Console.WriteLine(loadSlurList());
             await client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
 
-        public static string[] loadFile()
+        public static string[] loadSlurList()
         {
             string[] slurList = System.IO.File.ReadAllLines(@"BANNED WORDS TEXT FILE LOCATION");
             return (slurList);
+        }
+        
+        public static string[] loadWatchList()
+        {
+            string[] watchList = System.IO.File.ReadAllLines(@"WATCHED WORD LIST TEXT FILE LOCATION");
+            return (watchList);
         }
 
         private Task Log(LogMessage msg)
@@ -54,15 +60,17 @@ namespace VentTrent
             string censoredMessage = message.Content;
             if (message.Channel is SocketDMChannel && message.Author.Id != BOT ID)
             {
-                for (int i = 0; i < loadFile().Length; i++)
+                for (int i = 0; i < loadSlurList().Length; i++)
                 {
-                    censoredMessage = Regex.Replace(censoredMessage, loadFile()[i], "#########", RegexOptions.IgnoreCase);
-                    //censoredMessage = censoredMessage.Replace(loadFile()[i], "#########");
+                    censoredMessage = Regex.Replace(censoredMessage, loadSlurList()[i], "#########", RegexOptions.IgnoreCase);
                 }
                 
-                if ((censoredMessage.ToLower().Contains("kill myself") | censoredMessage.ToLower().Contains("suicide") | censoredMessage.ToLower().Contains("end it"))&& message.Channel is SocketDMChannel)
+                for (int i = 0; i < loadWatchList().Length; i++)
                 {
-                    await message.Channel.SendMessageAsync("ANTI SUICIDE MESSAGE");
+                    if (message.Content.ToLower().Contains(loadWatchList()[i]) && (message.Channel is SocketDMChannel))
+                    {
+                        await message.Channel.SendMessageAsync("ANTI SUICIDE MESSAGE");
+                    }
                 }
 
                 var eb = new EmbedBuilder()
